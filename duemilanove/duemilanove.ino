98eb620cbe016a121e1fe67294e41289d6dfba72
Kristina Heidinger <kristina.heidinger@gmail.com>
2016-01-25 22:27:36 +0100

    Added additional code for scale processing

--- duemilanove/duemilanove.ino
+++ duemilanove/duemilanove.ino
@@ -1,19 +1,32 @@
+#include <HX711.h>
+
 #include <SoftwareSerial.h>
 #include <Stepper.h>
 
+
 #define STEPS_PER_MOTOR_REVOLUTION 32
 #define STEPS_PER_OUTPUT_REVOLUTION 32*64
+
+#define DOUT  3
+#define CLK  2
 
 Stepper stepper(STEPS_PER_MOTOR_REVOLUTION, 6, 8, 7, 9);
 int stepsToTake;
 
 SoftwareSerial XBee(10, 11);
+
+float calibration_factor = -7050;
+HX711 scale(DOUT, CLK);
+
 void setup() {
     XBee.begin(9600);
     Serial.begin(9600);
     while (!Serial) {
       // wait  
     }
+
+    scale.set_scale(calibration_factor);
+    scale.tare();
 }
 void loop() {
  /* stepper.setSpeed(1);
@@ -21,10 +34,16 @@ void loop() {
   stepper.step(stepsToTake);
   delay(2000);
 */
+
+    // scale:
+  Serial.print("scale: ");
+  Serial.print(scale.read_average(), 5);
+  Serial.println();
+
   stepsToTake = STEPS_PER_OUTPUT_REVOLUTION / 2;
   stepper.setSpeed(500);
   stepper.step(stepsToTake);
-  delay(2000);
+  //delay(500);
 /*
   stepsToTake = - STEPS_PER_OUTPUT_REVOLUTION / 2;
   stepper.setSpeed(500);
@@ -40,4 +59,7 @@ void loop() {
       //char c = XBee.read();
       //Serial.write(c);
   //}
+
+
+  
 }
